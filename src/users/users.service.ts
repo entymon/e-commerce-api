4a239db64a947488a13d01e55893e144acbab3f6
entymon <pawel.olejniczak.i@gmail.com>
2024-10-13 18:10:22 +0200

    add signup and multi-part interceptor applied

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { CreateUserRequest } from './dto/create-user.request';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUserRequest) {
-    return 'This action adds a new user';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: CreateUserRequest): Promise<Pick<User, 'email' | 'id' | 'name'>> {
+    try {
+      return await this.prismaService.user.create({ 
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: {
+          email: true,
+          id: true,
+          name: true,
+          password: false,
+        } 
+      });
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new UnprocessableEntityException('User with this email already exists');
+      }
+      throw error
+    }
   }
 }
